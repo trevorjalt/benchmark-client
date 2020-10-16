9e1830d3701dd396f753fec9c3394d7c6fed6360
Trevor Alt <trevorjalt@gmail.com>
2020-10-15 23:07:45 -0400

    config fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,15 @@
+let apiPath
+let tokenKey;
+
+if (process.env.NODE_ENV === 'production') {
+    apiPath = 'https://still-fjord-98440.herokuapp.com/api'
+    tokenKey = 'benchmark-client-auth-token'
+} else {
+    apiPath = 'http://localhost:8000/api'
+    tokenKey = 'benchmark-client-auth-token'
+}
+
 export default {
-    API_ENDPOINT: 'https://still-fjord-98440.herokuapp.com/api',
-    TOKEN_KEY: 'benchmark-client-auth-token',
+    API_ENDPOINT: apiPath,
+    TOKEN_KEY: tokenKey,
 }
