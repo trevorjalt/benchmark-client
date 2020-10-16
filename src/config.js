15775785677abc2f762d8014d695f6229bd0ba6d
Trevor Alt <trevorjalt@gmail.com>
2020-10-15 22:19:51 -0400

    resolving issues

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://still-fjord-98440.herokuapp.com',
+    API_ENDPOINT: 'https://still-fjord-98440.herokuapp.com/api',
     TOKEN_KEY: 'benchmark-client-auth-token',
 }
