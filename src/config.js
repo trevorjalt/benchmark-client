4b2b30da011d73b8b8789130f2cc7059db47aa95
Trevor Alt <trevorjalt@gmail.com>
2020-10-15 22:15:34 -0400

    workout out bugs

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://still-fjord-98440.herokuapp.com/',
+    API_ENDPOINT: 'https://still-fjord-98440.herokuapp.com',
     TOKEN_KEY: 'benchmark-client-auth-token',
 }
