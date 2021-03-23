0b8e1c2db32aaf0412e9efe0c6d70fe7cc36d3c5
Trevor J Alt <trevor@trevorjalt.com>
2021-03-23 19:25:49 -0400

    deploy update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 const endpoints = {
-    API_ENDPOINT: 'https://still-fjord-98440.herokuapp.com/api',
+    API_ENDPOINT: 'https://benchmark-app-api.herokuapp.com/api',
     TOKEN_KEY: process.env.REACT_APP_API_TOKEN,
 }
 
